3bbe6f471b5a9354fde5749da453fdb26de5bfed
trieulinh <nguyenthuytrieulinh@gmail.com>
2023-06-20 00:00:30 +0700

    Fixed bài tập ForArrays

---
+++ src/ForArrays.java
@@ -0,0 +1,9 @@
+public class ForArrays {
+    public static void main(String[] args) {
+        int A[] = new int [25];
+        int j=0;
+        for (int i=0; i<=50; i=i+2) {
+            A[j] = i;
+            System.out.println(A[j]);
+        }}}
+
